ba3e96b8080d920e4f7cd95bf87a04f4b9cdd287
ruhailavp <ruhailaharis123@gmail.com>
2026-07-28 10:00:57 +0530

    Assignments

---
+++ Assignments/Day3/Assignments.string.js
@@ -0,0 +1,22 @@
+//Example 1
+
+let str="Hello World"
+let arr=str.split(" ")
+console.log(arr[arr.length-1].length)
+
+//Example 2
+
+let strn=" fly me to the moon "
+let trimmed=strn.trim()
+let arr2=trimmed.split(" ")
+console.log(arr2[arr2.length-1].length)
+
+//Example 3--Write a function to check if two strings are anagrams.
+
+let firstword="Silent"
+
+firstword.forEach((element) => {
+    console.log(element)
+
+})
+
